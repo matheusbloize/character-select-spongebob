40a504f3db4353296b852ed77da935b921a00f49
matheusbloize <mbloizepd@gmail.com>
2022-05-18 12:10:27 -0300

    teste

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+/* 
+    Objetivo 1 - quando passar o mouse em cima do personagem temos que:
+
+    - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele
+    - retirar a classe selecionado do personagem que está selecionado
+
+    Objetivo 2 - quando passar o mouse em cima do personagem da listagem, trocar a imagem e nome do personagem grande
+
+    -alterar a imagem do jogador 1
+    -alterar o nome do jogador 1
+*/
+
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        const idSelecionado = personagem.attributes.id.value;
+
+        if(idSelecionado === 'siri') return;
+
+        //objetivo 1
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+        
+        personagem.classList.add('selecionado');
+
+        //objetivo 2
+        
+        const imagemJogador1 = document.getElementById('personagem-jogador-1');
+        imagemJogador1.src = `./src/images/${idSelecionado}.png`;
+
+        const nomeJogador1 = document.getElementById('nome-jogador-1');
+        const nomeSelecionado = personagem.getAttribute('data-name');
+
+        nomeJogador1.innerHTML = nomeSelecionado;
+    });
+});
